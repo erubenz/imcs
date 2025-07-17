db3284a551779093fa520b947e7d139c11c1019e
erubenz <81283580+erubenz@users.noreply.github.com>
2025-07-17 20:42:54 +0400

    Add environment configuration

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDzy8HfnUv4PNSl3DNW3wl1aC7FwdgMDwc",
-  authDomain: "imcs-20e60.firebaseapp.com",
-  projectId: "imcs-20e60",
-  storageBucket: "imcs-20e60.appspot.com",
-  messagingSenderId: "201844564603",
-  appId: "1:201844564603:web:b9bcda834cd8f1edebb8de"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
